4b25f35bc80cdb1ae294e3b4c50befed1b11b13b
NoamHome365 <noam@home365.co>
2023-01-11 18:04:55 +0200

    applicant scoring - data-applicant-scoring

--- src/data_applicant_scoring/main.py
+++ src/data_applicant_scoring/main.py
@@ -11,7 +11,7 @@ from src.data_applicant_scoring.app import execute
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
-logger.addHandler(watchtower.CloudWatchLogHandler())
+# logger.addHandler(watchtower.CloudWatchLogHandler())
 
 router = APIRouter()
 
@@ -24,7 +24,7 @@ app = FastAPI(
 def healthcheck():
     return {"status": "ok"}
 
-@app.post("/data_applicant_scoring")
+@app.post("/data-applicant-scoring")
 async def data_applicant_scoring(request: Request):
     body = await request.json()
     response = execute(body)
@@ -36,3 +36,17 @@ async def data_applicant_scoring(request: Request):
 app.include_router(router, prefix="/data-applicant-scoring")
 
 logger.info("Starting up")
+
+if __name__ == '__main__':
+    jst = {"applicant_id": 'BC5T2A09-ELE2-4HG0-AE74-4E2DF78A3E1D',
+            "credit_score": 678,
+            "eviction": 2,
+            "criminal": 1,
+            "age": 28
+    }
+    body_json = json.dumps(jst)
+
+    response, json_vendors, state = execute(jst)
+    thread = threading.Thread(target=dbh.execute_to_db, kwargs={
+        'jsono': response, 'jsoni': jst, 'output_vendors': json_vendors, 'state': state})
+    thread.start()
